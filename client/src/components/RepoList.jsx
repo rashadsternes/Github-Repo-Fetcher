87ef3d0119403247514b3c8b1346b1b7e61ba490
NickRashad <nrsternes@gmail.com>
2019-04-19 00:38:36 -0500

    Complete displaying top 25 repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,7 +4,13 @@ const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    <ul>
+      {props.repos.map((repo) =>
+        <li key={repo.id_repo.toString()}>{repo.name}</li>
+      )}
+    </ul>
   </div>
+
 )
 
 export default RepoList;
