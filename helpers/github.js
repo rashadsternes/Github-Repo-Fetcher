8ccb55a6145c0dc0247c738fac25b2de6720f8c1
NickRashad <nrsternes@gmail.com>
2019-04-19 13:40:04 -0500

    Complete Bare Minimum Requirements

--- helpers/github.js
+++ helpers/github.js
@@ -1,12 +1,12 @@
 const request = require('request');
-// const config = require('../config.js');
+const config = require('../config.js');
 
 let getReposByUsername = (user, callback) => {
   let options = {
     url: `https://api.github.com/users/${user}/repos`,
     headers: {
-      'User-Agent': process.env.USERNAME, // config.USERNAME,
-      'Authorization': `token ${process.env.TOKEN}` // `token ${config.TOKEN}`
+      'User-Agent': config.USERNAME, // process.env.USERNAME, // config.USERNAME,
+      'Authorization': `token ${config.TOKEN}`, // `token ${process.env.TOKEN}` // `token ${config.TOKEN}`,
     }
   };
   request.get(options, (err, response, body) => {
