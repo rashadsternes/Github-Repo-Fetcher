e518ee9d57a2de57d34683723204ba774f1eac0f
NickRashad <nrsternes@gmail.com>
2019-04-20 22:07:11 -0500

    Update env config variables

--- helpers/github.js
+++ helpers/github.js
@@ -6,7 +6,7 @@ let getReposByUsername = (user, callback) => {
     url: `https://api.github.com/users/${user}/repos`,
     headers: {
       'User-Agent': process.env.USERNAME, // config.USERNAME,
-      'Authorization': `token ${process.env.TOKEN}` // `token ${config.TOKEN}`,
+      'Authorization': 'token ' + process.env.TOKEN, // `token ${config.TOKEN}`,
     }
   };
   request.get(options, (err, response, body) => {
@@ -19,8 +19,8 @@ let getColloborators = (url, callback) => {
   let options = {
     url: url,
     headers: {
-      'User-Agent': config.USERNAME, // process.env.USERNAME, // config.USERNAME,
-      'Authorization': `token ${config.TOKEN}`, // `token ${process.env.TOKEN}` // `token ${config.TOKEN}`,
+      'User-Agent': process.env.USERNAME, // process.env.USERNAME, // config.USERNAME,
+      'Authorization': 'token ' + process.env.TOKEN, // `token ${process.env.TOKEN}` // `token ${config.TOKEN}`,
     }
   };
   request.get(options, (err, response, body) => {
