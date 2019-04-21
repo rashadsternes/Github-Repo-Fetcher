fbf9754324de117fa061945723a3c07f473fd331
NickRashad <nrsternes@gmail.com>
2019-04-20 21:23:29 -0500

    Minor update to db logic

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 // mongoose.connect('mongodb://localhost/fetcher', {useNewUrlParser: true });
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI);
 
 
 mongoose.set('useCreateIndex', true);
